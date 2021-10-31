597ebb04ed8b6a34b8212b12e99d676741b41d6a
Dextro007 <vivek.kumar007656@gmail.com>
2021-10-31 22:33:31 +0530

    DP fibonacci

--- Dynamic Programming/fibonacci.cpp
+++ Dynamic Programming/fibonacci.cpp
@@ -1,24 +1,25 @@
-#include<iostream>
-#include<climits>
-#include<vector>
+// Usual method to compute fibnonacci
 
-using namespace std;
-
-unsigned int calcMaxIncSubset(vector<int> arr){
-
-}
-
-int main(){
-	
-	int temp;
-	int n;
-	cin>>n;
-	vector<int> arr(n);
-	for(int i = 0; i<n;i++){
-		cin>>temp;
-		arr.push_back(temp);
-	}
-	cout<< calcMaxIncSubset(arr);
-	return 0;
-
-}
+class Solution
+{
+    public:
+    //Function to return list containing first n fibonacci numbers.
+    vector<long long> printFibb(int n) 
+    {
+	    vector<long long> result;
+	    long long a = 1;
+	    long long b = 1;
+	    long long c = 0;
+	    result.push_back(a);
+	    if(n == 1)
+		    return result;
+	    result.push_back(b);
+	    for(long long i  = 2; i<n; i++){
+		    c = a+b;
+		    a = b;
+		    b = c;
+		    result.push_back(c);
+    	}
+	    return result;
+    }
+};
