bce2771dfbb31db36991affb90c1d76c15c2b3fe
Dextro007 <vivek.kumar007656@gmail.com>
2021-11-07 20:25:03 +0530

    started Longesst common subsequence

---
+++ Dynamic Programming/longestCommonSubsequence.cpp
@@ -0,0 +1,9 @@
+class Solution
+{
+    public:
+    //Function to find the length of longest common subsequence in two strings.
+    int lcs(int x, int y, string s1, string s2)
+    {
+        // your code here
+    }
+};
